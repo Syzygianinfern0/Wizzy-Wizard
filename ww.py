36921663c0752ec83c587bcd324874adef20b45e
Syzygianinfern0 <spsharan2000@gmail.com>
2019-03-29 17:47:18 +0530

    Made the green Mask

---
+++ ww.py
@@ -0,0 +1,27 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+ 
+while(True):
+    # Reading the video
+
+    ret, frame = cap.read()
+    cv2.imshow('ORIGINAL', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):                               # The quit line
+        break
+    
+    # Mask to find the colour
+
+    frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)                  # The convertion 
+    cv2.imshow('HSV', frame_hsv)
+
+    l_green = np.array([40, 40, 40])
+    u_green = np.array([70, 255, 255])
+    green_mask = cv2.inRange(frame_hsv, l_green, u_green)               # The mask
+    cv2.imshow('Green Mask', green_mask)
+
+
+
+cap.release()
+cv2.destroyAllWindows()
