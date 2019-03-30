a32d089daf95d4924dfe6c7608b43c503590ad57
Syzygianinfern0 <spsharan2000@gmail.com>
2019-03-30 17:33:33 +0530

    Perfected Rectangle with DP algo

--- Bonus.py
+++ Bonus.py
@@ -8,23 +8,21 @@ import pyautogui
 def main():
     flag = 0
     cap = cv2.VideoCapture(0)
-    
+    screenCnt = []
     while True:
         # Reading the video
 
         _, frame = cap.read()
-        frame = cv2.flip(frame, 1)
+        frame = cv2.flip(frame, 1)                                  # Flip for mirror
         
-
-
-        blurred_frame = cv2.GaussianBlur(frame, (5, 5), 0)
+        blurred_frame = cv2.GaussianBlur(frame, (5, 5), 0)          # Blurrd
         hsv = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
 
-        sensitivity = 100
+        sensitivity = 120
         lower_white = np.array([0,0,255-sensitivity])
         upper_white = np.array([255,sensitivity,255])
 
-        mask = cv2.inRange(hsv, lower_white, upper_white)
+        mask = cv2.inRange(hsv, lower_white, upper_white)           # Whites mask
         cv2.imshow('whites', mask)
 
         # Noise Removal
@@ -32,30 +30,27 @@ def main():
         kernel = np.ones((5, 5), np.uint8)
         no_noise = cv2.erode(mask, kernel, iterations = 1)           # Noise Removal
 
-        perfect = cv2.morphologyEx(no_noise, cv2.MORPH_OPEN, kernel)       # Morphing to enhance
+        perfect = cv2.morphologyEx(no_noise, cv2.MORPH_OPEN, kernel)                            # Morphing to enhance
         
-        contours, _ = cv2.findContours(perfect, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
- 
-        for contour in contours:
-            # area = cv2.contourArea(contour)
-    
-            # if area > 20000:
-            #     cv2.drawContours(frame, contour, -1, (0, 255, 0), 3)
-            box = cv2.boundingRect(contour)
-            x, y, w, h = box
+        cnts = cv2.findContours(perfect.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)     # find contours in the thresholded image
+        cnts = imutils.grab_contours(cnts)
 
-            if w<200 or h<200:
-                continue
+        cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:2]
 
-            # cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 10, 16)
+        for c in cnts:
+            peri = cv2.arcLength(c, True)
+            approx = cv2.approxPolyDP(c, 0.1 * peri, True)
 
-            cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
-            cv2.circle(frame, (x+w, y), 5, (0, 255, 0), -1)
-            cv2.circle(frame, (x, y+h), 5, (0, 255, 0), -1)
-            cv2.circle(frame, (x+w, y+h), 5, (0, 255, 0), -1)
+            if len(approx) == 4:
+                print('something')
+                screenCnt = approx
+                cv2.drawContours(frame, [screenCnt], -1, (0, 255, 0), 2)    
+                break
+                
 
-            # cv2.drawContours(frame, contour, -1, (0, 255, 0), 3)
-            
+        # cv2.drawContours(frame, screenCnt, -1, (0, 255, 0), 2)    
+        # cv2.drawContours(frame, approx, -1, (0, 255, 0), 2)    
+
         cv2.imshow('ORIGINAL', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):                               # The quit line
