af466da0bc66519a076163be706218825acc6395
Syzygianinfern0 <spsharan2000@gmail.com>
2019-03-30 20:18:21 +0530

    Perfected by some colour derp

--- Bonus.py
+++ Bonus.py
@@ -58,21 +58,21 @@ def main():
         top_left_x, top_left_y = screenCnt[1][0][0], screenCnt[1][0][1]
         top_right_x, top_right_y = screenCnt[0][0][0], screenCnt[0][0][1]
         bottom_left_x, bottom_left_y = screenCnt[2][0][0], screenCnt[2][0][1]
+        bottom_right_x, bottom_right_y = screenCnt[3][0][0], screenCnt[3][0][1]
 
-        print(top_left_x, top_left_y)
 
-        pts1 = np.float32([[640, 0], [0, 0], [640, 480]])             #  3 points of the image
-        pts2 = np.float32([[top_left_x, top_left_y], [top_right_x, top_right_y], [bottom_left_x, bottom_left_y]])    # To map to corr coordintes of cam feed
+        pts1 = np.float32([[640, 0], [0, 0], [640, 480], [0, 480]])             #  3 points of the image Note the mirror flip in the beginning
+        pts2 = np.float32([[top_left_x, top_left_y], [top_right_x, top_right_y], [bottom_left_x, bottom_left_y], [bottom_right_x, bottom_right_y]])    # To map to corr coordintes of cam feed
 
-        M = cv2.getAffineTransform(pts1, pts2)
-        dst = cv2.warpAffine(img, M, (cols, rows))
+        M, _ = cv2.findHomography(pts1, pts2)
+        dst = cv2.warpPerspective(img, M, (cols, rows))
 
         cv2.imshow('dst', dst)
-        finished = cv2.addWeighted(dst, 0.5, frame, 0.5, 0)
 
+        white_rem_mask = cv2.bitwise_not(mask)
+        mask_rem_feed = frame * cv2.cvtColor(white_rem_mask, cv2.COLOR_GRAY2RGB)
 
-
-        # finished = cv2.bitwise_or(dst, screenCnt)
+        finished = cv2.bitwise_or(mask_rem_feed, dst)
 
         cv2. imshow('Finished', finished)
         if cv2.waitKey(1) & 0xFF == ord('q'):                               # The quit line
